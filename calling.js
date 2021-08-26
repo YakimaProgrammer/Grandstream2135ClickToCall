f8370765ef03edfb137461be5f042c0a7aca9331
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2021-08-25 21:28:01 -0700

    Cleaned up the code and allowed for detecting DOM changes

--- calling.js
+++ calling.js
@@ -1,53 +1,29 @@
 const PROTOCOL = "http";
-const IP = "192.168.1.204";
+const IP = "192.168.1.153";
 const START_CALL = "/cgi-bin/api-make_call";
-const LOGIN = "/cgi-bin/dologin";
 const USERNAME = "admin";
 const PASSWORDS = ["Admin", "admin"];
 
-async function sha256(message) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message);                    
-
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-    // convert bytes to hex string                  
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-}
+//Build the request url based on the constants above.
+const URL = PROTOCOL + "://" + IP + START_CALL;
 
 async function callNumber(number) {
-
-	
 	for (var password of PASSWORDS) {
-			//first, login
-			/*
-			await fetch(PROTOCOL + "://" + IP + LOGIN + "?phonenumber=" + number +
-					"&account=" + USERNAME + "&password=" + password, {  
-				method: 'POST', // or 'PUT'
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					username: USERNAME,
-					password: await sha256(password)
-				}),
-				credentials: 'include'
-			});
-		*/
-		
-		var callUrl = PROTOCOL + "://" + IP + START_CALL + "?phonenumber=" + number +
-			"&account=" + USERNAME + "&password=" + password + "&account=0";	
-			
-		var resp = await fetch(callUrl, {
+        
+        //Do I need to use URLSearchParams?
+        //No.
+        //But it makes everything easier to read.
+        var params = new URLSearchParams()
+        
+        params.add("phonenumber", number);
+        params.add("account", USERNAME);
+        params.add("password", password);
+	    
+        //Now for the moment we've all been waiting for! Send off the request!
+		var resp = await fetch(URL + "?" + params.toString(), {
 			method: "GET",  
 			credentials: 'include', 
-			mode: 'no-cors',
-			cookie: "session-role=admin;session-identity=51049240e1629943912;"
+			mode: 'no-cors'
 		});
 		
 		if (resp.status === 200) {
@@ -56,5 +32,5 @@ async function callNumber(number) {
 	}
 }
 
-
+//Listen for when the user clicks on a telephone number link
 chrome.runtime.onMessage.addListener(msg => callNumber(msg.number));
--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var anchors = document.getElementsByTagName("a");
-
-for(let i = 0; i < anchors.length; i++) {
-    if(anchors[i].getAttribute("href").startsWith("tel:")) {
-        let tel = anchors[i].getAttribute("href").split(":").pop();
-        console.log("Grandstream CTC: Replaced " + tel);
-        anchors[i].setAttribute("href", "#");
-
-        anchors[i].addEventListener("click", function () {
-            console.log("Grandstream CTC: Calling " + tel);
-			chrome.runtime.sendMessage({number: tel});
-        });
+function updateAnchors() {
+    for(var anchor of document.getElementsByTagName("a")) {
+        //Is this a phone number?
+        if (anchor.href.startswith("tel:")) {
+            //First, extract the phone number part
+            let tel = anchor.href.split(":").pop();
+            
+            //Then set the href to '#' so the webbrowser won't try to open the link on click
+            anchor.href = "#";
+            
+            //Then pass the number off to the extension's background script
+            //This allows me to access http:// sites from https://
+            anchor.addEventListener("click", () => chrome.runtime.sendMessage({number: tel}));
+        }
     }
 }
+
+//Check the page to see if there are any links I can update
+window.addEventListener("load", updateAnchors);
+
+//Now, watch for DOM changes in the future and respond by rechecking for unupdated anchors
+const observer = new MutationObserver(updateAnchors);
+window.addEventListener("load", () => observer.observe(
+    document.body,
+    {/* attributes: true, childList: true, */ subtree: true }
+);
